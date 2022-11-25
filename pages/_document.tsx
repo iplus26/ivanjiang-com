c98933cbd65bc2f996912a95cc0275380d0ac65a
Ivan Jiang <iplus26@gmail.com>
2022-11-25 23:15:43 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,14 @@ export default function Document() {
   return (
     <Html>
       <Head crossOrigin="anonymous">
-      <script src="https://res.wx.qq.com/d/journal/_next/static/chunks/8206.2a32b9d934b47b32.js" crossOrigin="anonymous"></script>
+        <script src="https://res.wx.qq.com/d/journal/_next/static/chunks/8206.2a32b9d934b47b32.js" crossOrigin="anonymous"></script>
+        <script src="https://res.wx.qq.com/d/vconsole/3.14.6/vconsole.min.js"></script>
+          <script dangerouslySetInnerHTML={{
+            __html: `
+           if (navigator.userAgent.indexOf('Mobile') > -1) {
+            var vConsole = new window.VConsole();
+           }`,
+          }}/>
       </Head>
       <body>
         123
