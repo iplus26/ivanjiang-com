57c38f0c8b2d68169f48d26416a73aee345d32e5
Ivan Jiang <iplus26@gmail.com>
2022-11-25 23:09:17 +0800

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head crossOrigin="anonymous">
+      <script src="https://res.wx.qq.com/d/journal/_next/static/chunks/8206.2a32b9d934b47b32.js" crossOrigin="anonymous"></script>
+      </Head>
+      <body>
+        123
+      <Main/>
+      <NextScript crossOrigin="anonymous"/>
+      </body>
+</Html>
+)
+}
